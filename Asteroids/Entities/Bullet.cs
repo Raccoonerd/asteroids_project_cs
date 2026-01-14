157e43be33da6eb57ab022305633f6e7ec53fe45
Krzysztof Miarka <krzeslaw42@gmail.com>
2026-01-14 19:12:34 +0100

    Implement Bullet class with lifespan and velocity handling

--- Asteroids/Entities/Bullet.cs
+++ Asteroids/Entities/Bullet.cs
@@ -6,7 +6,35 @@ namespace Asteroids
 {
     public class Bullet : Entity
     {
+        private float _lifespan = 1.5f; // seconds
+        private float _speed = 800f;
         public Bullet(Texture2D texture, Vector2 startPos, float rotation) : base(texture, startPos)
-        {}
+        {
+            _rotation = rotation;
+
+            // Setting initial velocity based on rotation    
+            Vector2 direction = new Vector2(
+                (float)Math.Cos(_rotation - MathHelper.PiOver2),
+                (float)Math.Sin(_rotation - MathHelper.PiOver2)
+            );
+            
+            _velocity = direction * _speed;
+
+            _radius = 2f;
+        }
+
+        public override void Update(GameTime gameTime, Viewport viewport)
+        {
+            float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            _lifespan -= deltaTime;
+
+            if(_lifespan <= 0f)
+            {
+                isExpired = true;
+            }
+
+            base.Update(gameTime, viewport);
+        }
     }
 }
